a3ed4a63d47c1d05f60445b4fdce9d621e604074
yamamotohiroshidai <yamamotohiroshidai@192.168.11.4>
2022-12-14 22:23:43 +0900

    [Add]カテゴリー 新規登録機能

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.entity.Category;
 import com.example.demo.entity.User;
 import com.example.demo.service.CategoryService;
 
@@ -24,5 +27,22 @@ public class CategoryController {
 		mav.addObject("loginUser", loginUser);
 		mav.setViewName("/categorytop");
 		return mav;
+	}
+	
+	//カテゴリー 新規登録画面表示
+	@GetMapping("/newcategory")
+	public ModelAndView newCategory() {
+		ModelAndView mav = new ModelAndView();
+		Category category = new Category();
+		mav.addObject("category", category);
+		mav.setViewName("/categoryNew");
+		return mav;
+	}
+	
+	//ユーザー登録処理
+	@PostMapping("/createcategory")
+	public ModelAndView createCategory(@ModelAttribute("category") Category category) {
+		categoryService.saveCategory(category);
+		return new ModelAndView("/categoryTop");
 	}
 }
--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -3,11 +3,17 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Category;
 import com.example.demo.repository.CategoryRepository;
 
 @Service
 public class CategoryService {
 	@Autowired
 	CategoryRepository categoryRepository;
+	
+	//レコード追加
+	public void saveCategory(Category category) {
+		categoryRepository.save(category);
+	}
 
 }
