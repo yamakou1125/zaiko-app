2fa0c692a9349ad27b600b355175a0ab40d2a982
yamamotohiroshidai <yamamotohiroshidai@192.168.11.4>
2022-12-21 22:16:08 +0900

    [Add]アイテム 新規登録機能

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -51,5 +58,56 @@ public class ItemController {
         //遷移先を指定
 		mav.setViewName("/new");
 		return mav;
+	}
+	
+	//アイテム追加処理
+	@PostMapping("/additem")
+	public ModelAndView addItem(@ModelAttribute("itemModel") Item item, BindingResult result, int userId,
+			@RequestParam(name = "category") int categoryId, @RequestParam(name = "expirationDate", required = false) String expirationDate) {
+
+		ModelAndView mav = new ModelAndView();
+		List<String> errorMessages = new ArrayList<String>();
+
+		String name = item.getName();
+
+		//バリデーション(アイテム名)
+		if (Strings.isBlank(name)) {
+			errorMessages.add("アイテム名を入力してください");
+		}
+
+		//バリデーション(カテゴリー)
+		if (categoryId == 0) {
+			errorMessages.add("カテゴリーを選択してください");
+		}
+
+
+		//期限
+		if (Strings.isBlank(expirationDate)) {
+			errorMessages.add("期限を入力してください");
+		}else {
+			expirationDate += " 00:00:00";
+			Timestamp tm = Timestamp.valueOf(expirationDate);
+			item.setExpirationDate(tm);
+		}
+
+		//エラーメッセージ数をチェック
+		if (errorMessages.size() != 0) {
+			mav.setViewName("/new");
+			mav.addObject("errorMessages", errorMessages);
+			List<Category> categoryList = itemService.findAllCategory();
+			//カテゴリリストをセット
+	        mav.addObject("categoryList", categoryList);
+	        //カテゴリリスト初期値をセット
+	        mav.addObject("selectedCategory", categoryId);
+	        mav.addObject("accrualDate", expirationDate);
+			return mav;
+		}
+
+		item.setUserId(userId);
+		item.setCategoryId(categoryId);
+
+		itemService.saveItem(item);
+
+		return new ModelAndView("redirect:/");
 	}
 }
--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Category;
+import com.example.demo.entity.Item;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.ItemRepository;
 
@@ -19,5 +23,17 @@ public class ItemService {
 	//カテゴリーリスト取得
 	public List<Category> findAllCategory() {
 		return categoryRepository.findAll();
+	}
+	
+	//レコード追加
+	public void saveItem(Item item) {
+		item.setCreatedAt(Timestamp.valueOf(getTime()));
+		item.setUpdatedAt(Timestamp.valueOf(getTime()));
+		itemRepository.save(item);
+	}
+
+	//日時取得
+	private String getTime() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 	}
 }
